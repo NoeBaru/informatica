c215323bba3a6dd1ec4e8d6ef381d687604fb435
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2023-09-27 11:44:00 +0200

    compiti per domani in corso

---
+++ compitiDelleVacanze/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+#define P 10 //numero di punteggi e prove
+#define DIM 100 //dimensione massima
+#define STRL 20 //lunhezza stringhe
--- ripassoTerza/14.c
+++ ripassoTerza/14.c
@@ -6,30 +6,39 @@
 #define P 10 //numero di punteggi e prove
 #define DIM 100 //dimensione massima
 #define STRL 20 //lunhezza stringhe
+#define NOME_FILE "concorso.txt" //nome file
 
 /*
 Author: Noemi Baruffolo
-date: 26/09/2023
-es. 30
+date: /08/2023
+es. 14
 text:
-In un concorso, i partecipanti sono sottoposti a 10 prove. I risultati del concorso sono memorizzati 
-in un array di strutture, che contiene, per ogni concorrente, i seguenti dati:
-• nome: stringa contenente al massimo 20 caratteri, compreso il terminatore;
+In un concorso, i partecipanti sono sottoposti a 10 prove. I risultati del
+concorso sono memorizzati in un array di strutture, che contiene, per ogni
+concorrente, i seguenti dati:
+• nome: stringa contenente al massimo 20 caratteri, compreso il
+terminatore;
 • punteggi: una sequenza di 10 interi.
-• totale: calcola la somma dei punteggi durante il caricamento e verrà usato per la classifica
-Caricare i dati da tastiera e visualizzare per ciascuna prova, chi è il vincitore e con quale punteggio 
-e quindi la classifica finale.
+• Totale: totale punteggi
+Si scriva un programma che stampi, per ciascuna prova, chi è il vincitore e
+con quale punteggio (ci sono pari merito). Inoltre si visualizzi la
+classifica finale.
+Ad esempio:
+Rossi 4 6 1 ...
+Bianchi 12 0 9 ...
+Verdi 2 3 7 ...
+Il programma dovrà visualizzare:
+prova 1: Bianchi 12
+prova 2: Rossi 6
+prova 3: Bianchi 9
 Esempio di contenuto dell'array di struct:
-{"Rossi", {4,6,1,2,0,4,0,1,2,2}},
-{"Bianchi", {12,0,9,4,5,2,1,1,0,5}},
-{"Verdi", {2,3,7,9,10,4,5,9,7,2}},
-{"Astolfi", {0,2,6,1,8,5,9,10,7,8}},
-{"Lorenzi", {6,9,7,1,0,0,4,5,2,1}},
-{"Franchi", {1,10,11,19,4,7,2,0,2,7}}
+{"Rossi", {4,6,1,2,0,11,0,2,3,3},32},
+{"Bianchi", {12,0,9,4,5,2,1,1,0,5},39},
+{"Verdi", {2,3,7,9,10,4,5,9,7,2},58},
+{"Astolfi", {0,2,6,1,8,5,9,10,6,8},55},
+{"Lorenzi", {6,9,7,1,0,0,4,5,5,1},38},
+{"Franchi", {1,10,11,19,4,7,2,0,4,7},65}
 */
-
-//vedi es14 compiti delle vacanze
-//concorsoEs30.txt
 
 typedef struct {
     char nome[STRL];
@@ -113,13 +122,8 @@ void stampaClassifica(Concorso conc[], int dim) {
 
 int main() {
     Concorso concorrenti[DIM];
-    char nomeFile[STRL];
 
-    printf("Inserisci il nome del file: ");
-    fflush(stdin);
-    scanf("%s", nomeFile);
-
-    int num = caricaDati(concorrenti, DIM, P, nomeFile); //numero di partecipanti e caricamento dei dati da file
+    int num = caricaDati(concorrenti, DIM, P, NOME_FILE); //numero di partecipanti e caricamento dei dati da file
 
     winner(concorrenti, num, P); //vincitore per ogni prova
 
