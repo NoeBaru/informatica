6417d8d9ce7e9b3abcb9742e43715c98e638a6ea
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2023-10-10 10:33:07 +0200

    Ho aggiunto l'es di esmpio in Java svolto oggi

---
+++ Java/esMostro/src/Mostro.java
@@ -0,0 +1,10 @@
+public class Mostro {
+    private String nome;
+    private int vita;
+    private int forza;
+
+    private static final int MIN_VITA = 0;
+    private static final int MAX_VITA = 100;
+    private static final int MIN_FORZA = 5;
+    private static final int MAX_FORZA = 10;
+}
---
+++ Java/esMostro/src/Test.java
@@ -0,0 +1,6 @@
+public class Test { //utilizzata dal mostro
+    public static void main(String[] args) { //pswm e crea il main
+        Mostro mio; //occupa lo spazio di una variabile di tipo oggetto
+        mio = new Mostro; ///metodo costruttore, ho creato il mostro che occupa spazio in memoria
+    }
+}
