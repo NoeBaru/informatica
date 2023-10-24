c5f2f6b939d0f8f9372bfbf1c9cfe3ef769346c7
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2023-10-24 23:54:25 +0200

    Add es16_nominativo homework exercise for tomorrow (25/10/2023)

--- Java/es16_nominativo/src/Nominativo.java
+++ Java/es16_nominativo/src/Nominativo.java
@@ -1,2 +1,43 @@
 public class Nominativo {
+    private char cognome;
+    private char nome;
+    private int giorno;
+    private int mese;
+    private int anno;
+
+    public boolean omocodice;
+
+    private final int ANNO_ATTUALE = 2023;
+    private char sesso;
+
+    public Nominativo(){
+
+    }
+    public void setNomeCognome(){
+
+    }
+    public String getNomeCognome(){
+        return cognome;
+    }
+    public void setSesso(){
+
+    }
+    public String getSesso(){
+        return sesso;
+    }
+    public void setData(){
+
+    }
+    public int getData(){
+        return giorno + mese + anno;
+    }
+    public char iniziali(){
+        return caratteri;
+    }
+    public char IniziailiCF() {
+        return inizialiCF;
+    }
+    public boolean eOmocodice(){
+        return omocodice;
+    }
 }
--- Java/es16_nominativo/src/Test.java
+++ Java/es16_nominativo/src/Test.java
@@ -1,2 +1,22 @@
+/*
+author: Noemi baruffolo
+date: 24/10/2023
+es: 16 nominativo
+text:
+Scrivere una classe Java Nominativo che possiede gli attributi cognome, nome, giorno mese e anno di nascita, sesso
+e oltre ai soliti set/get e toString, anche i metodi:
+• iniziali() che restituisce: le prime tre lettere del nome in minuscolo concatenate con le prime tre lettere del
+cognome in maiuscolo. Utilizzare il metodo concat ed il metodo substring. Se non ci sono 3 lettere aggiungere
+delle X finali.
+
+• inizialiCF() che restituisce i primi 11 caratteri del Codice Fiscale in maiuscolo (cercare su internet le regole).
+• eOmocodice (Nominativo altroNom) che restituisce true se l'oggetto su cui è invocato genera lo stesso CF
+dell'oggetto altroNom passato per parametro
+*/
+
 public class Test {
+    public static void main(String[] args) {
+
+    }
+
 }
